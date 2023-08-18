e2e87d9ee89755da5571248dd1cd0b6275f6cf2c
bora <nhatbaoxxd@gmail.com>
2023-08-19 03:19:41 +0700

    Installation and usage Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Stack } from '@mui/material'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import { pink } from '@mui/material/colors'
+import HomeIcon from '@mui/icons-material/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Trung Quan dev</h1>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+      <Box
+        sx={{
+          '& > :not(style)': {
+            m: 2,
+          },
+        }}
+      >
+        <HomeIcon />
+        <HomeIcon color="primary" />
+        <HomeIcon color="secondary" />
+        <HomeIcon color="success" />
+        <HomeIcon color="action" />
+        <HomeIcon color="disabled" />
+        <HomeIcon sx={{ color: pink[500] }} />
+      </Box>
     </>
   )
 }

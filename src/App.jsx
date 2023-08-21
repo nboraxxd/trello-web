e4d0fc80471a8d0c2a8586e232725b62c451859e
bora <nhatbaoxxd@gmail.com>
2023-08-21 18:21:03 +0700

    Dark mode & light mode in MUI p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { Stack } from '@mui/material'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import Box from '@mui/material/Box'
-import { pink } from '@mui/material/colors'
 import HomeIcon from '@mui/icons-material/Home'
+import { Stack } from '@mui/material'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import { pink } from '@mui/material/colors'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>Trung Quan dev</div>
       <Typography variant="body" color="text.secondary">
         h1. Heading

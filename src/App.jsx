16d54c47a0b865b5044379e155f5783363f1d895
bora <nhatbaoxxd@gmail.com>
2023-08-22 20:47:17 +0700

    Dark mode & light mode in MUI p2

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,54 @@ import Typography from '@mui/material/Typography'
 import { pink } from '@mui/material/colors'
 import { useColorScheme } from '@mui/material/styles'
 
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize="small" /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize="small" /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessOutlinedIcon fontSize="small" /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
+
   return (
     <Button
       onClick={() => {
@@ -22,6 +68,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
       <hr />
       <div>Trung Quan dev</div>

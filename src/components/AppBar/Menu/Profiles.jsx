0d5358854313266e140844797a9c43952c62a497
bora <nhatbaoxxd@gmail.com>
2023-08-26 15:20:37 +0700

    Trello App Bar (part 1)

---
+++ src/components/AppBar/Menu/Profiles.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import { Logout, PersonAdd, Settings } from '@mui/icons-material'
+
+export default function Profiles() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <Box>
+      <IconButton
+        onClick={handleClick}
+        size="small"
+        sx={{ padding: 0 }}
+        aria-controls={open ? 'account-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+      >
+        <Avatar
+          sx={{ width: 30, height: 30 }}
+          src="https://trungquandev.com/wp-content/uploads/2023/08/transparent-main-avatar-circle-min-trungquandev-codetq@3x-Large.jpeg"
+          alt="Avatar"
+        />
+      </IconButton>
+      <Menu
+        id="basic-menu-recent"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-recent',
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{width: 28, height: 28, mr: 2}} /> Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{width: 28, height: 28, mr: 2}} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,18 +1,60 @@
 import Box from '@mui/material/Box'
-import ModeSelect from '@/components/ModeSelect/index'
+import ModeSelect from '@/components/ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import SvgIcon from '@mui/material/SvgIcon'
+import { ReactComponent as TrelloIcon } from '@/assets/trello.svg'
+import Typography from '@mui/material/Typography'
+import Workspaces from './Menu/Workspaces'
+import Recent from './Menu/Recent'
+import Starred from './Menu/Starred'
+import Templates from './Menu/Templates'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Badge from '@mui/material/Badge'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import Tooltip from '@mui/material/Tooltip'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import Profiles from './Menu/Profiles'
 
 export default function AppBar() {
   return (
     <Box
+      px={2}
       sx={{
         display: 'flex',
         alignItems: 'center',
+        justifyContent: 'space-between',
         width: '100%',
         height: (theme) => theme.trello.appBarHeight,
-        backgroundColor: 'primary.light',
       }}
     >
-      <ModeSelect />
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main' }} />
+          <Typography variant="span" sx={{ fontSize: '1.125rem', fontWeight: 'bold', color: 'primary.main' }}>
+            Trello
+          </Typography>
+        </Box>
+        <Workspaces />
+        <Recent />
+        <Starred />
+        <Templates />
+        <Button variant="outlined">Outlined</Button>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <TextField id="outline-search" label="Search..." type="search" size="small" />
+        <ModeSelect />
+        <Tooltip title="Notification">
+          <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+        <Tooltip title="Help">
+          <HelpOutlineIcon sx={{ cursor: 'pointer' }} />
+        </Tooltip>
+        <Profiles />
+      </Box>
     </Box>
   )
 }
